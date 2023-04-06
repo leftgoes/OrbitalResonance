2f57f559ee5012a03dd19b24e5658e5ba4713f20
Ha Jong Kim <yluminance@gmail.com>
2023-04-06 02:48:29 +0200

    Converting between Keplerian Orbit Elements and Cartesian State Vectors for Planets and Particles

---
+++ StarSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace OrbitalResonance
+{
+    class StarSystem
+    {
+        Star mainStar;
+        List<Planet> planets;
+
+        public StarSystem(double starMass)
+        {
+            mainStar = new(starMass);
+            planets = new();
+        }
+
+        public void AddPlanet(Planet planet)
+        {
+            planets.Add(planet);
+        }
+    }
+}
---
+++ Vector3D.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrbitalResonance
+{
+    internal class Vector3D
+    {
+        public double x, y, z;
+
+        public Vector3D(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public override string ToString()
+        {
+            return $"[{x}, {y}, {z}]";
+        }
+
+        public static Vector3D operator +(Vector3D u, Vector3D v)
+        {
+            return new(u.x + v.x, u.y + v.y, u.z + v.z);
+        }
+
+        public static Vector3D operator -(Vector3D u, Vector3D v)
+        {
+            return new(u.x - v.x, u.y - v.y, u.z - v.z);
+        }
+
+        public static Vector3D operator *(double s, Vector3D v)
+        {
+            return new(s * v.x, s * v.y, s * v.z);
+        }
+
+        public static Vector3D operator /(Vector3D v, double s)
+        {
+            return new(v.x/s, v.y/s, v.z/s);
+        }
+
+        public static Vector3D Cross(Vector3D u, Vector3D v)
+        {
+            return new(u.y * v.z - u.z * v.y,
+                       u.z * v.x - u.x * v.z,
+                       u.x * v.y - u.y * v.x);
+        }
+
+        public static double Dot(Vector3D u, Vector3D v)
+        {
+            return u.x * v.x + u.y * v.y + u.z * v.z;
+        }
+
+
+        public static Vector3D Zero { get { return new(0, 0, 0); } }
+
+        public double Magnitude { get { return Math.Sqrt(x * x + y * y + z * z); } }
+
+        public Vector3D Normalized { get { return this / Magnitude; } }
+    }
+}
